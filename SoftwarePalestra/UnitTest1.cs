3df0373edcb76cd364f410c9a3d2c179fa1d3bfc
michiburro14@gmail.com <michiburro14@gmail.com>
2021-03-16 13:14:26 +0100

    Add project files.

---
+++ SoftwarePalestra/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TariffePalestra;
+
+namespace SoftwarePalestra
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void OfferteTariffe()
+        {
+
+        }
+    }
+}
